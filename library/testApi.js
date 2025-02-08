c7c352b87e491f940447cf43cd854401b1c2a015
Yran1202 <yrangarciav@outlook.com>
2025-02-07 19:59:21 -0600

    actualizar fetch asynv

---
+++ library/testApi.js
@@ -0,0 +1,15 @@
+
+const { searchCountry } = require ('./library/searchCountry.js')
+
+async function run() {
+    const countries = await searchCountry(); 
+    console.log(countries); 
+}
+  
+  run();
+  
+
+
+
+
+
